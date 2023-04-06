b05d52c5c943ed9ab86d03ff9e2f79af692599e8
MBaez97 <marcelobaezparaguay@gmail.com>
2023-04-05 22:10:18 -0400

    Add Babel in code

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log('Hello World!');
+(async function App() {
+    const main = null || document.getElementById('main');
+    main.innerHTML = `
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <h1>Graph</h1>
+                    <div id="graph"></div>
+                </div>
+            </div>  
+        </div>
+    `
+    const graph = null || document.getElementById('graph');
+})()
