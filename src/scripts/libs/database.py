43dbc0fbac31f0ae3c07152de67bd2a8735532eb
MBaez97 <marcelobaezparaguay@gmail.com>
2023-04-06 23:53:29 -0400

    add python_scripts

---
+++ src/scripts/libs/database.py
@@ -0,0 +1,6 @@
+# Libreria donde le paso el query y automaticamente me retorna los datos
+# Tambien Conexiones al Mongo o al Redis
+import psycopg2
+from redis import Redis
+from pymongo import MongoClient
+
---
+++ src/scripts/libs/lib_manejo_csv.py
@@ -0,0 +1,19 @@
+import csv
+
+
+def escribe_csv(archivo, data):
+    with open(archivo, "a+", newline="") as out_file:
+        writer = csv.writer(out_file)
+        writer.writerow(data)
+
+
+def crea_csv(archivo, data):
+    with open(archivo, "w+", newline="") as out_file:
+        writer = csv.writer(out_file)
+        writer.writerow(data)
+
+
+def lee_csv(archivo, delimiter=","):
+    with open(archivo, "r", newline="") as in_file:
+        reader = csv.reader(in_file, delimiter=delimiter)
+        return list(reader)[1:]
