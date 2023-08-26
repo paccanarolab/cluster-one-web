707af97b84210209254fccb9efa5b4d35d0a108a
MBaez97 <marcelobaezparaguay@gmail.com>
2023-08-26 17:13:39 -0400

    Clean code

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import './main.css';
 import Template from './templates/Template.js';
 
 (async function App() {
-  // console.log('App()');
   const main = null || document.getElementById('main');
   main.innerHTML = await Template();
 })();
