7ddb92719d62189cb3e8f65acc313f8de80cb3d3
MBaez97 <marcelobaezparaguay@gmail.com>
2023-08-27 15:34:20 -0400

    change styles

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-import "./styles/global.scss"
+import "./styles/global.scss"; // Import global styles and apply them to the entire app
 
 
 ReactDOM.render(<App />, document.getElementById("app"));
