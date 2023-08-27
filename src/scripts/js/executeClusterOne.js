edd70ed873903fc3f96d17a0d80ceeedb9ae7292
MBaez97 <marcelobaezparaguay@gmail.com>
2023-08-27 19:08:58 -0400

    change style name and add scripts to execute Cluster one

--- src/scripts/js/executeClusterOne.js
+++ src/scripts/js/executeClusterOne.js
@@ -2,6 +2,46 @@
 import './main.css';
 import cytoscape from 'cytoscape';
 
+const uploadFilePpi = async () => {
+  const fileInput = document.createElement('input');
+  fileInput.type = 'file';
+  fileInput.accept = '.csv, .txt';
+
+  fileInput.onchange = async (event) => {
+      const file = event.target.files[0];
+      const formData = new FormData();
+      formData.append('file', file);
+
+      try {
+          const response = await fetch('YOUR_ENDPOINT_URL', {
+              method: 'POST',
+              body: formData
+          });
+          
+          const data = await response.json();
+          console.log(data);
+      } catch (error) {
+          console.error("There was an error uploading the file:", error);
+      }
+  };
+
+  fileInput.click();
+}
+
+const byIdPpi = async (id) => {
+  try {
+      const response = await fetch(`YOUR_ENDPOINT_URL/${id}`, {
+          method: 'GET'
+      });
+      
+      const data = await response.json();
+      console.log(data);
+  } catch (error) {
+      console.error("There was an error fetching the data:", error);
+  }
+}
+
+
 Promise.all([
   fetch('./cy-style.json')
     .then(function(res) {
@@ -266,6 +306,8 @@ Promise.all([
     });
 
   });
+
+
 
 
 
