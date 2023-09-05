9b569003b70a394c3c6311415574c730745b29d0
MBaez97 <marcelobaezparaguay@gmail.com>
2023-09-05 16:29:36 -0400

    add graph in app

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-import "./styles/global.scss"; // Import global styles and apply them to the entire app
-
 
 ReactDOM.render(<App />, document.getElementById("app"));
