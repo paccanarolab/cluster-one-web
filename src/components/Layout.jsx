04b3479da702a7410961b2f561c55718dffb0618
MBaez97 <marcelobaezparaguay@gmail.com>
2023-09-06 00:09:26 -0400

    static layouts

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,20 +1,115 @@
 import React from "react";
+import { AppContext } from "./AppContext.jsx";
+
+const AvailableLayouts = [
+    {
+        name: "random",
+        fit: true,
+        // circle: true,
+        directed: true,
+        padding: 50,
+        // spacingFactor: 1.5,
+        animate: true,
+        animationDuration: 1000,
+        avoidOverlap: true,
+        nodeDimensionsIncludeLabels: false
+    },
+    {
+        name: "grid",
+        fit: true,
+        padding: 30,
+        animate: true,
+        animationDuration: 1000,
+        avoidOverlap: true,
+        nodeDimensionsIncludeLabels: false
+    },
+    {
+        name: "circle",
+        fit: true,
+        padding: 30,
+        sort: function(a, b){ return a.degree() - b.degree(); },
+        animate: true,
+        animationDuration: 1000,
+        avoidOverlap: true,
+        nodeDimensionsIncludeLabels: false
+    },
+    {
+        name: "concentric",
+        fit: true,
+        minNodeSpacing: 50,
+        equidistant: false,
+        animate: true,
+        animationDuration: 1000,
+        avoidOverlap: true,
+        nodeDimensionsIncludeLabels: false
+    },
+    {
+        name: "breadthfirst",
+        fit: true,
+        directed: true,
+        padding: 30,
+        animate: true,
+        animationDuration: 1000,
+        avoidOverlap: true,
+        nodeDimensionsIncludeLabels: false
+    },
+    {
+        name: "cose",
+        fit: true,
+        padding: 50,
+        animate: true,
+        animationDuration: 1000,
+        avoidOverlap: true,
+        nodeDimensionsIncludeLabels: false
+    },
+    {
+        name: "dagre",
+        fit: true,
+        padding: 30,
+        animate: true,
+        animationDuration: 1000,
+        avoidOverlap: true,
+        nodeDimensionsIncludeLabels: false
+    },
+    {
+        name: "spread",
+        fit: true,
+        animate: true,
+        animationDuration: 1000,
+        avoidOverlap: true,
+        nodeDimensionsIncludeLabels: false
+    },
+    {
+        name: "euler",
+        fit: true,
+        padding: 30,
+        animate: true,
+        animationDuration: 1000,
+        avoidOverlap: true,
+        nodeDimensionsIncludeLabels: false
+    },
+    {
+        name: "klay",
+        fit: true,
+        padding: 50,
+        animate: true,
+        animationDuration: 1000,
+        avoidOverlap: true,
+        nodeDimensionsIncludeLabels: false
+    }
+   // Puedes agregar más layouts si lo necesitas
+];
 
 const Layout = ({classname}) => {
-    const [layout, setLayout] = React.useState('');
-
-    const handleLayoutChange = (event) => {
-        setLayout(event.target.value);
-    }
-
+    const { layout, handleChangeLayout } = React.useContext(AppContext);
     return (
         <div>
-            <select value={layout} onChange={handleLayoutChange} className={classname}>
-                <option value="layout1">Layout 1</option>
-                <option value="layout2">Layout 2</option>
-                <option value="layout3">Layout 3</option>
-                <option value="layout4">Layout 4</option>
-                {/* Puedes agregar más opciones si lo necesitas */}
+            <select value={layout} onChange={handleChangeLayout} className={classname}>
+                {AvailableLayouts.map((layout, index) => (
+                    <option key={index} value={layout.name}>
+                        {layout.name}
+                    </option>
+                ))}
             </select>
         </div>
     );
