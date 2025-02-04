7f03af5f5a71fd15196e89a8b119610db56a75c0
Mbaez97 <marcelobaezparaguay@gmail.com>
2025-02-04 17:36:00 -0300

    I have changed the name of downladed image and also i put better resolution

--- src/components/DownloadGraph.jsx
+++ src/components/DownloadGraph.jsx
@@ -6,15 +6,17 @@ import "../styles/DownloadButton.scss";
 const DownloadGraph = ({ cy, name}) => {
     const { 
         cyGraph,
+        organismName,
+        ppiLabel,
     } = React.useContext(AppContext);
     const handleDownload = () => {
       if (cy) {
-        const pngDataUrl = cy.png({ full: true, scale: 8.3333, bg: 'white' });
+        const pngDataUrl = cy.png({ full: true, scale: 8.3333, bg: 'white', maxWidth: 1920, maxHeight: 1080 });
   
         // Create a temporary link element
         const downloadLink = document.createElement('a');
         downloadLink.href = pngDataUrl;
-        downloadLink.download = `cl1_${cyGraph.file_id}_graph.png`;
+        downloadLink.download = `cl1_${organismName.substring(0, 20) + "..."}_${ppiLabel}_complex_${cyGraph.file_id}_graph.png`;
   
         // Append the link to the body
         document.body.appendChild(downloadLink);
